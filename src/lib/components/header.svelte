30a717d5b5c26dd2a9bba59dc5e38ad37bd908c2
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2024-02-22 12:13:58 +0300

    update header

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -3,11 +3,22 @@
 	import SettingsIcon from '$lib/icons/settingsIcon.svelte';
 </script>
 
-<div
-	class="flex w-full justify-between items-center px-9 py-3 bg-tertiary-50/50 shadow-md shadow-tertiary-100"
->
-	<button on:click={() => goto('/')} class="btn text-4xl font-black text-primary-400"
-		>Splash
+<div class="flex relative w-full justify-between items-center md:px-16 px-8 z-50 bg-inherit">
+	<button
+		on:click={() => {
+			console.log('on click');
+		}}
+	>
+		<img src="/cheqd-logo.png" class="md:h-10 h-5" />
 	</button>
-	<SettingsIcon class="w-9 h-9 text-tertiary-500" />
+	<div class="flex space-x-2 w-1/2 items-center justify-end">
+		<span class="hidden md:block">Already have an account?</span>
+		<div class="flex justify-end md:justify-start">
+			<button
+				class="btn bg-gradient-to-r from-primary-500 to-primary-400 text-white text-xl rounded-3xl flex"
+			>
+				<span>Sign in</span>
+			</button>
+		</div>
+	</div>
 </div>
