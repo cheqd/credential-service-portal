ab2c8b63d8d342b37a8d89660d6f2feac83c0933
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2024-02-22 12:48:11 +0300

    update home page

--- src/routes/(app)/home/+page.svelte
+++ src/routes/(app)/home/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import LoaderIcon from '$lib/icons/LoaderIcon.svelte';
+	let isSignOutLoading = false;
 	const handleSignout = async () => {
+		isSignOutLoading = true;
 		const response = await fetch('/api/logto/signout', {
 			method: 'DELETE',
 			redirect: 'manual'
@@ -9,11 +12,19 @@
 			sessionStorage.clear();
 			window.open(response.headers.get('Location')!, '_self');
 		}
-
+		isSignOutLoading = false;
 		console.error('signout error: ', await response.text());
 	};
 </script>
 
 <div class="w-full h-full flex justify-center items-center">
-	<button on:click={handleSignout}>Sign out</button>
+	<button
+		on:click={handleSignout}
+		class=" btn bg-gradient-to-r from-primary-500 to-primary-400 text-white text-xl rounded-2xl flex py-2"
+	>
+		{#if isSignOutLoading}
+			<LoaderIcon />
+		{/if}
+		<span>Sign out</span>
+	</button>
 </div>
