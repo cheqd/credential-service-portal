2de721432f083495d3fa9c22ce1f651836bb10be
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2024-02-23 14:44:12 +0300

    highlight current path on sidebar

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -2,12 +2,14 @@
 	import { goto } from '$app/navigation';
 	import BillingIcon from '$lib/icons/billingIcon.svelte';
 	import HomeIcon from '$lib/icons/HomeIcon.svelte';
+	import { page } from '$app/stores';
 	export let closeDrawer: () => void;
 </script>
 
 <div class="flex list-nav flex-col gap-6 p-4 items-start h-full shadow-md">
 	<a
-		class="btn tracking-wide rounded-lg hover:bg-tertiary-300 flex gap-2 justify-start items-center w-full text-start"
+		class="btn tracking-wide rounded-lg hover:bg-tertiary-300 flex gap-2 justify-start items-center w-full text-start {$page
+			.url.pathname === '/home' && 'bg-tertiary-300 lg:bg-primary-50'}"
 		href="/home"
 		on:click={() => {
 			goto('/home');
@@ -18,7 +20,8 @@
 		Home
 	</a>
 	<a
-		class="btn tracking-wide rounded-lg hover:bg-tertiary-300 flex gap-2 justify-start items-center w-full text-start"
+		class="btn tracking-wide rounded-lg hover:bg-tertiary-300 flex gap-2 justify-start items-center w-full text-start {$page
+			.url.pathname === '/billing' && 'bg-tertiary-300 lg:bg-primary-50'}"
 		href="/billing"
 		on:click={() => {
 			goto('/billing');
