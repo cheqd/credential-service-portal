07bd2f88a31394b8acfe677f972779b4ca106f2d
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2024-03-14 09:26:34 +0300

    refactor billing server side

--- src/routes/(app)/billing/+page.server.ts
+++ src/routes/(app)/billing/+page.server.ts
@@ -1,7 +1,25 @@
-import type { PageServerLoad } from "./$types";
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
-    const idToken = await locals.idToken;
-    return { idToken }
+	const idToken = await locals.idToken;
+	if (idToken) {
+		const products = await locals.credentialServiceBillingApi.listProducts(false, {
+			headers: {
+				'id-token': idToken
+			}
+		});
+		const subscriptions = await locals.credentialServiceBillingApi.listSubscription({
+			headers: {
+				'id-token': idToken
+			}
+		});
+		if (products.success && subscriptions.success) {
+			return {
+				products: products.data.products,
+				subscriptions: subscriptions.data.subscriptions
+			};
+		}
+	}
 
+	return { products: [], subscriptions: [] };
 };
