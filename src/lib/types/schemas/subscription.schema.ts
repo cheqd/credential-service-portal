b67a079bfbecba197e0948fd9e6553af268feace
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2024-03-21 14:12:05 +0300

    add update and create subscription schemas

--- src/lib/types/schemas/subscription.schema.ts
+++ src/lib/types/schemas/subscription.schema.ts
@@ -23,3 +23,22 @@ export const GetSubscriptionResponseSchema = z.object({
 		data: z.array(SubscriptionSchema)
 	})
 });
+
+export const CreateSubscriptionRequestBodySchema = z.object({
+	price: z.string(),
+	successURL: z.string(),
+	cancelURL: z.string(),
+	idempotencyKey: z.string().optional()
+});
+
+export const UpdateSubscriptionRequestBodySchema = z.object({
+	returnUrl: z.string()
+});
+
+const StripeSubscriptionResponseSchema = z.object({
+	clientSecrete: z.string()
+});
+
+export const UpdateSubscriptionResponseSchema = StripeSubscriptionResponseSchema;
+
+export const CreateSubscriptionResponseSchema = StripeSubscriptionResponseSchema;
