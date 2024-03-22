67f78b47df822524f8d48a7b3850957843d01cd6
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2024-03-22 13:35:50 +0300

    refactor billing page

---
+++ src/lib/api/subscriptions.ts
@@ -0,0 +1,91 @@
+import { env } from '$env/dynamic/public';
+
+import type {
+	CreateSubscriptionRequestBody,
+	CreateSubscriptionResponse,
+	UpdateSubscriptionRequestBody,
+	UpdateSubscriptionResponse
+} from '$lib/types/types/subscription.types';
+
+import type { CredentialServiceApiResponse, GenericErrorResponse } from './helpers';
+
+export async function createSubscription(
+	priceId: string,
+	idToken: string | null
+): Promise<CredentialServiceApiResponse<CreateSubscriptionResponse, GenericErrorResponse>> {
+	try {
+		const requestBody = {
+			price: priceId,
+			successURL: env.PUBLIC_STRIPE_CREATE_SUBSCRIPTION_SUCCESS_URL,
+			cancelURL: env.PUBLIC_STRIPE_CREATE_SUBSCRIPTION_CANCEL_URL
+		} satisfies CreateSubscriptionRequestBody;
+
+		const response = await fetch('/api/billing/subscription/create', {
+			method: 'POST',
+			body: JSON.stringify(requestBody),
+			headers: {
+				'id-token': idToken || ''
+			}
+		});
+
+		if (response.status !== 200) {
+			return {
+				error: await response.text(),
+				success: false,
+				status: response.status
+			};
+		}
+
+		const data = (await response.json()) as CreateSubscriptionResponse;
+		return {
+			data: data,
+			success: true,
+			status: response.status
+		};
+	} catch (e) {
+		return {
+			success: false,
+			error: (e as Error).message,
+			status: 500
+		};
+	}
+}
+
+export async function updateSubscription(
+	idToken: string | null
+): Promise<CredentialServiceApiResponse<UpdateSubscriptionResponse, GenericErrorResponse>> {
+	try {
+		const requestBody = {
+			returnUrl: env.PUBLIC_STRIPE_UPDATE_SUBSCRIPTION_RETURN_URL
+		} satisfies UpdateSubscriptionRequestBody;
+
+		const response = await fetch('/api/billing/subscription/update', {
+			method: 'POST',
+			body: JSON.stringify(requestBody),
+			headers: {
+				'id-token': idToken || ''
+			}
+		});
+
+		if (response.status !== 200) {
+			return {
+				error: await response.text(),
+				success: false,
+				status: response.status
+			};
+		}
+
+		const data = (await response.json()) as UpdateSubscriptionResponse;
+		return {
+			data: data,
+			success: true,
+			status: response.status
+		};
+	} catch (e) {
+		return {
+			error: (e as Error).message,
+			success: false,
+			status: 500
+		};
+	}
+}
--- src/routes/(app)/billing/+page.svelte
+++ src/routes/(app)/billing/+page.svelte
@@ -1,131 +1,92 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { createSubscription, updateSubscription } from '$lib/api/subscriptions';
 	import BillingPlanCard from '$lib/components/BillingPlanCard.svelte';
 	import CurrentPlanCard from '$lib/components/CurrentPlanCard.svelte';
 	import type { Product } from '$lib/types/types/product.types.js';
-	import type {
-		CreateSubscriptionRequestBody,
-		CreateSubscriptionResponse,
-		Subscription,
-		UpdateSubscriptionRequestBody,
-		UpdateSubscriptionResponse
-	} from '$lib/types/types/subscription.types.js';
+	import type { Subscription } from '$lib/types/types/subscription.types.js';
 	import { env as pubEnv } from '$env/dynamic/public';
-
-	import { onMount } from 'svelte';
 
 	export let data;
 
-	let products: Product[];
-	let currentSubscription: Subscription | null;
-	let currentPlan: Product;
+	let products: Product[] = [];
+	let currentSubscription: Subscription = null;
+	let currentPlan: Product | null = null;
 
-	export const createSubscription = async (priceId: string) => {
-		const requestBody = {
-			price: priceId,
-			successURL: 'http://localhost:3002/home',
-			cancelURL: 'http://localhost:3002/home'
-		} as CreateSubscriptionRequestBody;
-		const response = await fetch('/api/billing/subscription/create', {
-			method: 'POST',
-			body: JSON.stringify(requestBody),
-			headers: {
-				'id-token': data.idToken || ''
-			}
-		});
-		console.log('create sub res status', response.status);
-		// console.log('response data', await response.json());
-		const res = (await response.json()) as CreateSubscriptionResponse;
-		console.log('create sub response', res);
-		if (res.clientSecret) {
-			window.location.href = res.clientSecret;
-		}
-	};
-
-	export const updateSubscription = async (priceId: string) => {
-		const requestBody = {
-			returnUrl: 'http://localhost:3002/home'
-		} as UpdateSubscriptionRequestBody;
-		const response = await fetch('/api/billing/subscription/update', {
-			method: 'POST',
-			body: JSON.stringify(requestBody),
-			headers: {
-				'id-token': data.idToken || ''
-			}
-		});
-		console.log('create sub res status', response.status);
-		// console.log('response data', await response.json());
-		const res = (await response.json()) as UpdateSubscriptionResponse;
-		console.log('update sub response', res);
-		if (res.clientSecret) {
-			window.location.href = res.clientSecret;
-		}
-	};
-
-	$: console.log('is loading');
 	onMount(async () => {
-		products = data.products?.products.data;
-		currentSubscription = data.subscription;
-		console.log('current sub', currentSubscription);
-		console.log('data', data);
-		currentPlan = products.filter(
-			(p) => p.id === currentSubscription?.items.data[0].plan.product
-		)[0];
+		products = data?.products?.products.data ?? [];
+		currentSubscription = data?.subscription ?? null;
 
-		// TODO: sort products by current plan
+		if (currentSubscription) {
+			currentPlan =
+				products.find((p) => p.id === currentSubscription?.items.data[0]?.plan.product) || null;
+		}
 
-		console.log('current plan', currentPlan);
+		console.log('Current Plan:', currentPlan);
 	});
+
+	async function handleSubscription(priceId: string) {
+		try {
+			const idToken = data.idToken || null;
+			if (data.subscriptionNotFound) {
+				const createSub = await createSubscription(priceId, idToken);
+				if (createSub.success) {
+					window.location.href = createSub.data.clientSecret;
+				}
+			} else {
+				const updateSub = await updateSubscription(idToken);
+				if (updateSub.success) {
+					window.location.href = updateSub.data.clientSecret;
+				}
+			}
+		} catch (error) {
+			console.error('Subscription error:', error);
+		}
+	}
 </script>
 
-<div class=" h-full w-full flex flex-col gap-9">
+<div class="h-full w-full flex flex-col gap-9">
 	<div class="flex flex-col gap-24 p-9">
-		<div class="flex flex-col gap-6">
-			{#if products && currentSubscription}
-				<CurrentPlanCard
-					features={currentPlan.features.map((f) => {
-						return f.name;
-					})}
-					description={currentPlan.description}
-					title={currentPlan.name}
-				/>
-			{/if}
-			<div class="flex flex-col">
-				<span class="text-3xl tracking-wide font-semibold">All Plans</span>
-				<p class=" text-tertiary-900">Simple and transparent pricing that grows with you.</p>
-			</div>
-			<div class="no-scrollbar flex overflow-x-scroll space-x-8 p-4 justify-start -mx-8 lg:mx-0">
-				{#if products}
-					{#each products as p}
-						<BillingPlanCard
-							title={p.name}
-							description={p.description}
-							pricing={p.prices[0].unit_amount / 100}
-							featuresTitle={p.name.toLowerCase() === 'starter'
-								? 'Everything in Free trial and: '
-								: 'Everything in starter and: '}
-							features={p.features.map((f) => f.name)}
-							isCustom={p.name.toLowerCase() === 'custom'}
-							createSession={data.subscriptionNotFound ? createSubscription : updateSubscription}
-							isCurrentPlan={p.id === currentPlan.id && !data.subscriptionNotFound}
-							priceId={p.prices[0].id}
-							currency={p.prices[0].currency}
-						/>
-					{/each}
-				{/if}
-
-				<!--TODO: We may require a user tracking endpoint in the credential service to manage users with custom plans and their selected features.  -->
+		{#if currentPlan}
+			<CurrentPlanCard
+				features={currentPlan.features.map((f) => f.name)}
+				description={currentPlan.description}
+				title={currentPlan.name}
+			/>
+		{/if}
+		<div class="flex flex-col">
+			<span class="text-3xl tracking-wide font-semibold">All Plans</span>
+			<p class="text-tertiary-900">Simple and transparent pricing that grows with you.</p>
+		</div>
+		<div class="no-scrollbar flex overflow-x-scroll space-x-8 p-4 justify-start -mx-8 lg:mx-0">
+			{#each products as product}
 				<BillingPlanCard
-					title={pubEnv.PUBLIC_CUSTOM_BILLING_PLAN_TITLE}
-					description={pubEnv.PUBLIC_CUSTOM_BILLING_PLAN_DESCRIPTION}
-					pricing={pubEnv.PUBLIC_CUSTOM_BILLING_PLAN_PRICE}
-					featuresTitle={pubEnv.PUBLIC_CUSTOM_BILLING_PLAN_FEATURES_TITLE}
-					features={[]}
-					createSession={async () => {}}
-					isCustom={true}
-					priceId={''}
-					isCurrentPlan={false}
+					title={product.name}
+					description={product.description}
+					pricing={product.prices[0].unit_amount / 100}
+					featuresTitle={product.name.toLowerCase() === 'starter'
+						? 'Everything in Free trial and: '
+						: 'Everything in starter and: '}
+					features={product.features.map((f) => f.name)}
+					isCustom={product.name.toLowerCase() === 'custom'}
+					createSession={handleSubscription}
+					isCurrentPlan={product.id === currentPlan?.id && !data.subscriptionNotFound}
+					priceId={product.prices[0].id}
+					currency={product.prices[0].currency}
 				/>
-			</div>
+			{/each}
+
+			<BillingPlanCard
+				title={pubEnv.PUBLIC_CUSTOM_BILLING_PLAN_TITLE}
+				description={pubEnv.PUBLIC_CUSTOM_BILLING_PLAN_DESCRIPTION}
+				pricing={pubEnv.PUBLIC_CUSTOM_BILLING_PLAN_PRICE}
+				featuresTitle={pubEnv.PUBLIC_CUSTOM_BILLING_PLAN_FEATURES_TITLE}
+				features={[]}
+				createSession={async () => {}}
+				isCustom={true}
+				priceId={''}
+				isCurrentPlan={false}
+			/>
 		</div>
 	</div>
 </div>
