ca55f2400ee1f55b19d26e0dd40a33c834d59d11
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2024-03-27 15:27:43 +0300

    improve billing.server

--- src/routes/(app)/billing/+page.server.ts
+++ src/routes/(app)/billing/+page.server.ts
@@ -1,33 +1,54 @@
+import type { Subscription } from '$lib/types/types/subscription.types';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	try {
 		const idToken = await locals.idToken;
+		const { credentialServiceBillingApi } = locals;
 
 		if (idToken) {
-			const subscription = await locals.credentialServiceBillingApi.getCurrentSubscription({
+			const subscription = await credentialServiceBillingApi.getCurrentSubscription({
 				headers: {
 					'id-token': idToken
 				}
 			});
 
+			const subscriptionData: {
+				subscription: Subscription | null;
+				subscriptionNotFound: boolean;
+				idToken: string;
+				errorWhenGettingSubscription: boolean;
+			} = {
+				subscription: null,
+				subscriptionNotFound: false,
+				idToken,
+				errorWhenGettingSubscription: false
+			};
+
 			if (subscription.success) {
-				return {
-					subscription: subscription.data.subscription,
-					idToken,
-					subscriptionNotFound: false
-				};
+				subscriptionData.subscription = subscription.data.subscription;
 			} else if (subscription.status === 404) {
-				return {
-					subscriptionNotFound: true,
-					idToken
-				};
+				subscriptionData.subscriptionNotFound = true;
+			} else {
+				subscriptionData.errorWhenGettingSubscription = true;
 			}
+
+			return subscriptionData;
 		}
 
-		return { subscription: null, subscriptionNotFound: false, idToken };
+		return {
+			subscription: null,
+			subscriptionNotFound: false,
+			idToken,
+			errorWhenGettingSubscription: true
+		};
 	} catch (error) {
 		console.error('Error occurred while loading page:', error);
-		return { subscription: null, subscriptionNotFound: false };
+		return {
+			subscription: null,
+			subscriptionNotFound: false,
+			idToken: null,
+			errorWhenGettingSubscription: true
+		};
 	}
 };
